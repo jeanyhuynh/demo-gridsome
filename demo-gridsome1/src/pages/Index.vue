d213834709fb45e4e46baa24a5ae75d928fc9cc8
Trang Huynh <trang@construct.sg>
2019-11-01 10:43:03 +0700

    [master] add new sources

---
+++ demo-gridsome1/src/pages/Index.vue
@@ -0,0 +1,39 @@
+<template>
+  <Layout>
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+
+    <h1 class="test">Hello, world!</h1>
+
+    <p class="test">
+      Lorem ipsum dolor sit amet,
+      <span class="test1">hello</span> consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+    </p>
+    <ul>
+      <li>
+        <g-link to="/blog-html/blog-post-one">Blog post one</g-link>
+      </li>
+      <li>
+        <g-link to="/blog-html/blog-post-two">Blog post two</g-link>
+      </li>
+    </ul>
+    <p class="home-links">
+      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
+      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+    </p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!"
+  }
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
---
+++ demo-gridsome1/src/pages/Team.vue
@@ -0,0 +1,17 @@
+<template>
+  <BlogLayout>
+    <h1>Hello Team</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto at minima itaque, accusamus animi sit. Non, quos, suscipit officia doloribus quasi recusandae illo porro possimus voluptatem quod repellat hic totam.</p>
+  </BlogLayout>
+</template>
+
+<script>
+export default {
+  metaInfor: {
+    title: "Helo Team"
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
